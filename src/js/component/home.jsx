56b70877bd48d40f883de9aedda4d82e72c50aa5
cgabrielP <cgabriel2303@gmail.com>
2022-09-13 21:45:13 +0000

    contador

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,36 @@
 import React from "react";
-
+import PropTypes from 'prop-types';
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Home = () => {
+function SimpleCounter(props){
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container">
+			
+			<div className="four">{props.numeroCuatro %10}</div>
+			<div className="three">{props.numeroTres %10}</div>
+			<div className="two">{props.numeroDos %10}</div>
+			<div className="one">{props.numeroUno %10}</div>
 		</div>
 	);
 };
+SimpleCounter.propTypes={
+	numeroCuatro: PropTypes.number,
+	numeroTres: PropTypes.number,
+	numeroDos: PropTypes.number,
+	numeroUno: PropTypes.number,
+}
+let counter=0;
+setInterval(function(){
+	const four = Math.floor(counter/1000);
+	const three = Math.floor(counter/100);
+	const two = Math.floor(counter/10);
+	const one = Math.floor(counter/1);
+	counter++;
+	console.log(four, three,two, one)
+	
 
-export default Home;
+}, 1000)
+
+export default SimpleCounter;
--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,39 @@
 //import react into the bundle
+import PropTypes from "prop-types";
 import React from "react";
 import ReactDOM from "react-dom";
+
 
 // include your styles into the webpack bundle
 import "../styles/index.css";
 
-//import your own components
-import Home from "./component/home.jsx";
+ function SimpleCounter(props){
+	return (
+		<div className="container">
+			<div className="clock"><i class="fa-solid fa-clock"></i></div>
+			<div className="four">{props.numeroCuatro %10}</div>
+			<div className="three">{props.numeroTres %10}</div>
+			<div className="two">{props.numeroDos %10}</div>
+			<div className="one">{props.numeroUno %10}</div>
+		</div>
+	);
+};
+SimpleCounter.propTypes={
+	numeroCuatro: PropTypes.number,
+	numeroTres: PropTypes.number,
+	numeroDos: PropTypes.number,
+	numeroUno: PropTypes.number,
+}
+let counter=0;
+setInterval(function(){
+	const four = Math.floor(counter/1000);
+	const three = Math.floor(counter/100);
+	const two = Math.floor(counter/10);
+	const one = Math.floor(counter/1);
+	counter++;
+	console.log(four, three,two, one)
+	//render your react application
+ReactDOM.render(<SimpleCounter numeroUno= {one}numeroDos= {two} numeroTres= {three} numeroCuatro= {four}  />, document.querySelector("#app"));
+}, 1000)
 
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+
